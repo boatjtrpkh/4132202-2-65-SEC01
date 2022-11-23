ef8132312d15171941037c9b5d1d91ab20b45f72
boatjtrpkh <620112418083@bru.ac.th>
2022-11-23 11:42:10 +0700

    lab3

---
+++ LAB-3/main.js
@@ -0,0 +1,53 @@
+
+    var num =10;
+    let text ="boat";
+    //let Array =[10,15,"boat"];
+    const obj = { name: "boat",age: 22 };
+    const objMix = {
+        data:[
+            {name :"Dang",gpa:4.3,},
+            {name :"Dum",gpa:2.8,},
+        ],
+    };
+
+console.log(num);
+console.log(Array);
+console.log(objMix);
+
+let string = num + name;
+console.log(string);
+
+string = objMix.data[1].name + Array[0];
+console.log(string);
+
+string = "<p>" + Array[2] +"</p>";
+string = `<p>${Array}</p>`;
+
+console.log(string);
+
+function Add(a,b){
+    return
+}
+
+console.log(Add(3,5));
+
+document.getElementById("div1").innerHTML = name;
+$(function (){    
+   // alert("Hello");
+   $("#div1").html("Jittirin");
+
+   $("#bt1").click(function (){
+    $("#div1").html("I LOVE IT");
+   });
+
+   $("#bt3").click(() => { //arrow function
+    $(".in1").val("BRU");
+   });
+
+   $("#bt4").click(() => {
+    $(".in1:odd").addClass("red");
+   });
+
+   $("#div_about").load("./pages/about.html");
+
+});//jQuery Handle
